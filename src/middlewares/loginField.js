e811deeffb3641a6089f5a4c2266606cf2260fd3
Luanderson Alves <luaoderson@gmail.com>
2023-06-20 00:22:01 -0300

    fix: req03, added email validator by a community module

--- src/middlewares/loginField.js
+++ src/middlewares/loginField.js
@@ -1,8 +1,9 @@
+const validator = require('email-validator');
+
 const emailValidator = ({ body: { email } }, res, next) => {
-  const rEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
   if (!email) return next({ status: 400, message: 'O campo "email" é obrigatório' });
-  if (!rEmail.test(email)) {
-    return next({ status: 400, message: 'O "email" deve ter o formato "email@email.com"' }); 
+  if (!validator.validate(email)) {
+    return next({ status: 400, message: 'O "email" deve ter o formato "email@email.com"' });
   }
   return next();
 };
@@ -10,7 +11,7 @@ const emailValidator = ({ body: { email } }, res, next) => {
 const passValidator = ({ body: { password } }, res, next) => {
   if (!password) return next({ status: 400, message: 'O campo "password" é obrigatório' });
   if (password.length < 6) {
-    return next({ status: 400, message: 'O "password" deve ter pelo menos 6 caracteres' }); 
+    return next({ status: 400, message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
   next();
 };
