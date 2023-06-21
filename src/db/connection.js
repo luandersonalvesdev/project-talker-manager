5a815f85d802cebad42e1a9e0b0de8d6d7672ae7
Luanderson Alves <luaoderson@gmail.com>
2023-06-20 21:56:10 -0300

    fix: req12, environment variable

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,7 +1,7 @@
 const mysql2 = require('mysql2/promise');
 
 const connection = mysql2.createPool({
-  host: 'localhost',
+  host: process.env.MYSQL_HOSTNAME,
   port: 3306,
   password: 'password',
   database: 'TalkerDB',
